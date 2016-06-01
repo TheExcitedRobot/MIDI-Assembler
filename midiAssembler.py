c464dd0f39004fc28cc2f67e040b9bc65128d4e2
Cameron Dye <cameronkdye@gmail.com>
2016-05-31 22:07:09 -0400

    folder for sounds

--- midiAssembler.py
+++ midiAssembler.py
@@ -34,7 +34,7 @@ for c in xrange(1,len(midiSong.tracks)):
     #print "track len after",len(track)
 
     #Read in sound file
-    channelFile = "meow1_shaggy.wav"
+    channelFile = "./sounds/meow1_shaggy.wav"
     #sound = AudioSegment.from_wav(channelFile)
     from scipy.io import wavfile
     frame_rate, sound = wavfile.read(channelFile)
